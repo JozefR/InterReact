9c9225097ea0944584e78b9f6904a36c4b885e0a
jozefrandjak <randjakjozef@gmail.com>
2025-06-22 19:31:58 +0200

    add McpServer project with account tools functionality

---
+++ McpServer/Tools/AccountTools.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+using ModelContextProtocol.Server;
+
+namespace McpServer.Tools;
+
+[McpServerToolType]
+public class AccountTools
+{
+    [McpServerTool, Description("Get Account Summary.")]
+    public static async Task<string> GetAccountSummary()
+    {
+        return "";
+    }
+    
+    [McpServerTool, Description("Get Portfolio Value.")]
+    public static async Task<string> GetPortfolioValue()
+    {
+        return "";
+    }
+}
