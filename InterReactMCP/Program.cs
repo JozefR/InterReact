94d87f29bb5e8d3b266d9e8a433b8c071f4b9b72
jozefrandjak <randjakjozef@gmail.com>
2025-07-13 15:44:39 +0200

    working mcp test integration

--- InterReactMCP/Program.cs
+++ InterReactMCP/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+// Configure logging to output to the console
+builder.Services.AddLogging(logging =>
+{
+    logging.AddConsole();
+    logging.SetMinimumLevel(LogLevel.Debug);
+});
+
+// Configure the MCP server with standard input/output transport
+builder.Services.AddMcpServer()
+    .WithStdioServerTransport() // Ensure Claude Desktop supports this transport
+    .WithToolsFromAssembly();   // Ensure tools are in the same assembly
+
+var app = builder.Build();
+
+try
+{
+    // Run the application
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    // Log any unhandled exceptions
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred while running the application.");
+    throw;
+}
--- InterReactMCP/Tools/AccountTools.cs
+++ InterReactMCP/Tools/AccountTools.cs
@@ -1,20 +1,41 @@
 using System.ComponentModel;
+using System.Text.Json;
+using InterReact;
+using Microsoft.Extensions.Logging;
 using ModelContextProtocol.Server;
 
 namespace InterReactMCP.Tools;
 
 [McpServerToolType]
-public class AccountTools
+public sealed class AccountTools
 {
     [McpServerTool, Description("Get Account Summary.")]
     public static async Task<string> GetAccountSummary()
     {
-        return "";
+        // ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder
+        //     .AddSimpleConsole(c => c.SingleLine = true)
+        //     .SetMinimumLevel(LogLevel.Information));
+        //
+        // IInterReactClient client = await InterReactClient.ConnectAsync(options => options.LogFactory = loggerFactory);
+        //
+        // var positions = client.Service.GetAccountPositionsMultiAsync();
+        
+        var result = new
+        {
+            Event = "test",
+            Status = "ok"
+        };
+        return await Task.FromResult(JsonSerializer.Serialize(result));
     }
     
     [McpServerTool, Description("Get Portfolio Value.")]
     public static async Task<string> GetPortfolioValue()
     {
-        return "";
+        var result = new
+        {
+            Event = "test",
+            Status = "ok"
+        };
+        return await Task.FromResult(JsonSerializer.Serialize(result));
     }
 }
